147d8d54d246eabb97bc4377c5fc63a7875d16b5
AlberrDev <alberrdev@gmail.com>
2023-05-02 08:21:34 +0200

    Clase Persona 1 JuezLTI

--- src/clasepersona/ClasePersona.java
+++ src/clasepersona/ClasePersona.java
@@ -1,8 +1,64 @@
 package clasepersona;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class ClasePersona {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int casosCompletos = 0;
+        int casosNormales = 0;
+        String nombre = "";
+        String apellido = "";
+        String fechaNacimiento = "";
+        String fecha = "";
+        boolean hayFechaCorrecta = false;
+        Persona Gente = null;
+        casosCompletos = sc.nextInt();
+        String[] fechaArray;
+        casosNormales = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < casosCompletos; i++) {
+            nombre = sc.nextLine();
+            apellido = sc.nextLine();
+            fechaNacimiento = sc.nextLine();
+            try { //Cambiar fecha nacimiento
+                Gente = new Persona(nombre, apellido, fechaNacimiento);
+                fechaArray = Gente.getFechaNacimiento().split("[/-]");
+                        
+                System.out.println("Procesado: " + Gente.getNombre() + Gente.getApellidos() + "," + "nacido el " + fechaArray[0] + " del "
+                        + fechaArray[1] + " de " + fechaArray[2]);
+            } catch (IllegalArgumentException e) {
+                System.out.println("ERROR. Procesando siguiente persona");
+            }
+
+        }
+        for (int i = 0; i < casosNormales; i++) {
+            nombre = sc.nextLine();
+            apellido = sc.nextLine();
+            try {
+                Gente = new Persona(nombre, apellido);
+                System.out.println("Procesado: " + Gente.getNombre() + Gente.getApellidos() + "," + "nacido el " + Gente.getFechaNacimiento());
+            } catch (IllegalArgumentException e) {
+                System.out.println("ERROR. Procesando siguiente persona");
+            }
+
+        }
+        while (!hayFechaCorrecta) {
+            fecha = sc.nextLine();
+
+            try {
+                Gente.setFechaNacimiento(fecha);
+                hayFechaCorrecta = true;
+               System.out.println("Procesado: " + Gente.getNombre() + Gente.getApellidos() + "," + "nacido el " + Gente.getFechaNacimiento() + " del "
+                        + Gente.getFechaNacimiento() + " de " + Gente.getFechaNacimiento());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Fecha Incorrecta");
+            }
+
+        }
 
     }
 
--- src/clasepersona/Persona.java
+++ src/clasepersona/Persona.java
@@ -3,21 +3,28 @@ package clasepersona;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class Persona {
 
     private String nombre;
     private String apellidos;
     private LocalDate fechaNacimiento;
+    private DateTimeFormatter GUION = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    private DateTimeFormatter BARRAS = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public Persona(String nombre, String apellidos, String fechaNacimiento) {
 
-        if (ComprobarString(nombre) || ComprobarString(apellidos)) {
+        if (nombre.length() <= 0 || apellidos.length() <= 0) {
             throw new IllegalArgumentException();
         } else {
             this.nombre = nombre;
             this.apellidos = apellidos;
-
+            try {
+                this.fechaNacimiento = generarFecha(fechaNacimiento);
+            } catch (DateTimeException e) {
+                throw new IllegalArgumentException();
+            }
         }
 
     }
@@ -35,6 +42,40 @@ public class Persona {
 
     }
 
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFechaNacimiento(char separador) { //Cambiar fehca nacimiento por string con el formato
+        String fechaString = "";
+        LocalDate fechaLocalDate;
+        if (separador != '-' || separador != '/') {
+            throw new IllegalArgumentException();
+        } else if (separador == '-') {
+            fechaString = fechaNacimiento.format(GUION);
+        } else if (separador == '/') {
+            fechaString = fechaNacimiento.format(BARRAS);
+        }
+
+        return fechaString;
+    }
+
+    public String getFechaNacimiento() {
+        String fechaString = "";
+
+        if (this.fechaNacimiento == null) {
+            fechaString = null;
+        } else {
+            fechaString = this.fechaNacimiento.format(GUION);
+        }
+
+        return fechaString;
+    }
+
     public void setFechaNacimiento(String fechaNacimiento) throws IllegalArgumentException {
         this.fechaNacimiento = generarFecha(fechaNacimiento);
     }
@@ -44,19 +85,21 @@ public class Persona {
         int dia = 0;
         int mes = 0;
         int anyo = 0;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
         String[] fechaSeparada = fechaNacimiento.split("[-/]"); //Deberiam
         if (!fechaNacimiento.matches("[0-9]{2}[/][0-9]{2}[/][0-9]{4}") && !fechaNacimiento.matches("[0-9]{2}[-][0-9]{2}[-][0-9]{4}")) {
-            try {
-                dia = Integer.parseInt(fechaSeparada[0]);
-                mes = Integer.parseInt(fechaSeparada[1]);
-                anyo = Integer.parseInt(fechaSeparada[2]);
-                esFechaCorrecta = LocalDate.of(dia, mes, anyo);
+            throw new IllegalArgumentException();
+        }
+        try {
+            dia = Integer.parseInt(fechaSeparada[0]);
+            mes = Integer.parseInt(fechaSeparada[1]);
+            anyo = Integer.parseInt(fechaSeparada[2]);
+            esFechaCorrecta = LocalDate.of(anyo, mes, dia);
 
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException();
-            } catch (DateTimeException ex) {
-                throw new IllegalArgumentException();
-            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        } catch (DateTimeException ex) {
+            throw new IllegalArgumentException();
         }
 
         return esFechaCorrecta;
